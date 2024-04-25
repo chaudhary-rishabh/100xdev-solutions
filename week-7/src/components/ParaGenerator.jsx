7f6ae87b6868bc7d7175fcc2b76649d0fb826750
Rishabh <chaudharyrishabh029@gmail.com>
2024-04-25 12:27:01 +0530

    have to click double on submit to get result

--- week-7/src/components/ParaGenerator.jsx
+++ week-7/src/components/ParaGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ParaGenerator = () => {
 
@@ -17,14 +17,15 @@ const ParaGenerator = () => {
         for (let i = 0; i < number; i++) { 
             paragraph = paragraph + " " + paraSplit[i];
         }
-        setPara(paragraph)
+        setPara(paragraph);
     }
+
 
     return (
         <div style={{ height: "100vh", width: "100vw" }}>
             <h1 style={{textAlign:"center"}}>Para Generator</h1>
             <form onSubmit={generatePara} style={{margin:"auto", display:"flex", alignItems:"center", justifyContent:"center"}}>
-                <input style={{border:"1px solid black", padding:"10px"}} type="number" onChange={e => setText(e.target.value)} value={text} placeholder='Enter Number of Words' required />
+                <input style={{border:"1px solid black", padding:"10px"}} type="number" min={1} onChange={e => setText(e.target.value)} value={text} placeholder='Enter Number of Words' required />
                 <button style={{cursor:"pointer", border:"none", borderRadius:"20px", backgroundColor:"black", color:"white", padding:"10px"}} type='submit'>Submit</button>
             </form>
             <h1>Your {number} words paragraph is<br></br>  <b style={{ color: "gray", textShadow:"2px 0px 2px black"}}>{para}</b></h1>
