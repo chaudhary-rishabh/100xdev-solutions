6e6c76f0e648e218e1c28d77b42f24b88fd7cf05
Rishabh <chaudharyrishabh029@gmail.com>
2024-04-25 12:02:26 +0530

    assignment4 week7

--- week-7/src/pages/Home.jsx
+++ week-7/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Profile from '../components/Profile'
 import BackgroundChanger from '../components/BackgroundChanger'
+import ParaGenerator from '../components/ParaGenerator'
 
 const Home = () => {
   return (
     <div>
         {/* <Profile/> */}
-        <BackgroundChanger/>
+        {/* <BackgroundChanger/> */}
+        <ParaGenerator/>
     </div>
   )
 }
